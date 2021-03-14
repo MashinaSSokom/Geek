28a282d7d0480204e6b3c3c819d7cde1749ce0d7
MashinaSSokom <skltan@mail.ru>
2021-03-14 21:30:55 +0800

    added task_6_7

---
+++ Ashimov_Sultan_dz_6/task_6_7.py
@@ -0,0 +1,12 @@
+def main(start = None, data = None):
+    with open("bakery.csv", "r+", encoding="utf-8") as f:
+        text = f.readlines()
+        text[int(start)-1] = f'{data}\n'
+    with open("bakery.csv", "w", encoding="utf-8") as f:
+        f.writelines(text)
+
+
+if __name__ == '__main__':
+    import sys
+    start, data = sys.argv[1:]
+    exit(main(start, data))
